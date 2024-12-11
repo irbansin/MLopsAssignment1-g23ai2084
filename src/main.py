7faa9c243cf8ffbb8f095650f97d92b0a014d15c
irbansin <reach@anirbansinha.in>
2024-12-11 08:46:32 +0530

    implemented try block to handle error gracefuly

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-import asyncio
 
 import pandas as pd
 from flask import Flask, render_template, request, jsonify 
 import os
 from data_processor import convert_to_dataframe, train_model  # Import the data processing function
 from enums import TrainingModels
-from etl import etl
 
 
 app = Flask(__name__)
@@ -32,29 +30,29 @@ def upload_page():
 
 @app.route('/uploadfile', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'message': 'No file part in the request'}), 400
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({'message': 'No selected file'}), 400
-
-   
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(file_path)
-    df = pd.DataFrame()
-
     try:
-        df = convert_to_dataframe(file_path) # takes in file and trains a model
-        train_model(df, TrainingModels.RandomForestRegressor)
+        if 'file' not in request.files:
+            return jsonify({'message': 'No file part in the request'}), 400
 
-        return jsonify({'Model being Trained'}), 200
-    except Exception as e:
-        return jsonify({'message': f'Error processing file: {str(e)}'}), 500
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({'message': 'No selected file'}), 400
+
     
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        df = pd.DataFrame()
 
-    return jsonify({'message': 'Invalid file format. Please upload an Excel file.'}), 400
+        try:
+            df = convert_to_dataframe(file_path) # takes in file and trains a model
+            train_model(df, TrainingModels.RandomForestRegressor)
+
+            return jsonify({'Model being Trained'}), 200
+        except Exception as e:
+            return jsonify({'message': f'Error processing file: {str(e)}'}), 500
+    except:
+        return jsonify({'message': 'Invalid file format. Please upload an Excel file.'}), 400
 
 @app.route('/getPrediction', methods=['POST'])
 def predict():
